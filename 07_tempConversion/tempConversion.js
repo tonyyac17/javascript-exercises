30519a116f77f591df365f74e63ee2f674aa0746
Tony Yacovetta <tony.yacovetta@gmail.com>
2024-01-25 15:09:16 -0600

    solved tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreeF) {
+  let number = (degreeF - 32)/1.8;
+  let result = Math.round(number*10)/10;
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreeC) {
+  let number = (degreeC*1.8) + 32;
+  let result = Math.round(number * 10)/ 10;
+  return result;
 };
 
 // Do not edit below this line
